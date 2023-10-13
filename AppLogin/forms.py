6ac9cf00b5c0fbacedd0f89a0958d3cd36ed6da3
Abdullah-Sheehan <abdshe2005@gmail.com>
2023-10-14 04:39:03 +0600

    Create SignUp Form & Template

---
+++ AppLogin/forms.py
@@ -0,0 +1,40 @@
+from django import forms
+from django.forms import widgets
+from .models import User
+from django.contrib.auth.forms import UserCreationForm
+class SignUpForm(UserCreationForm):
+    choices = (
+        ('', "Account Type"),
+        ('1', 'Student'),
+        ('2','Teacher'),
+    )
+    type = forms.ChoiceField(label='', choices=choices, required=True)
+    email = forms.EmailField(label="", required=True, widget=forms.TextInput(attrs={
+        'placeholder':'Email Address',
+    }))
+    password1 = forms.CharField(required=True, label="", widget=forms.PasswordInput(attrs={
+        'placeholder':'Type your password'
+    }))
+    password2 = forms.CharField(required=True, label="", widget=forms.PasswordInput(attrs={
+        'placeholder':'Type your password again'
+    }))
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'type', 'password1', 'password2']
+        labels = {
+            'first_name': '',
+            'last_name':'',
+            'email': '',
+            'password1': '',
+            'password2': '',
+        }
+        widgets = {
+            'first_name': widgets.TextInput(attrs={
+                'placeholder':'First Name',
+                'autofocus':True
+            }),
+            'last_name': widgets.TextInput(attrs={
+                'placeholder':'Last Name'
+            }),
+
+        }
--- AppLogin/views.py
+++ AppLogin/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse, reverse_lazy
+from .forms import SignUpForm
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def signupUser(req):
-    return render(req, 'AppLogin/signupUser.html', context={})
+    form = SignUpForm()
+    return render(req, 'AppLogin/signupUser.html', context={'form':form})
 def loginUser(req):
     return render (req,'AppLogin/loginUser.html', context={})
 @login_required
